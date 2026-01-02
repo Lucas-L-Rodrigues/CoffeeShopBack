df17b1925516150f7da87685365f00aea14bc2a4
Lucas-L-Rodrigues <lucaslima.48a@gmail.com>
2026-01-02 14:48:05 -0300

    script de seed agora cria 2 usuários

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,13 +6,36 @@ const prisma = new PrismaClient()
 
 async function main() {
     // Limpar os dados que tem no BD
-    // await prisma.products.deleteMany()
+    await prisma.cartItem.deleteMany()
+    await prisma.cart.deleteMany()
+    await prisma.productVariant.deleteMany()
     await prisma.product.deleteMany()
-    // await prisma.categories.deleteMany()
     await prisma.category.deleteMany()
+    await prisma.user.deleteMany()
 
-    console.log("Banco de dados Limpo (produtos e categorias).\n");
-    // Criar categorias
+    console.log("\nBanco de dados Limpo\n");
+    // Criar usuario
+    await prisma.user.create({
+        data: {
+            id: 'ae05ca52-be37-4592-8629-06915a2469de',
+            name: 'Admin',
+            email: 'admin@gmail.com',
+            password_hash: 'senhaHash',
+            role: 'ADMIN',
+            carts: {}
+        }
+    })
+
+    await prisma.user.create({
+        data: {
+            id: 'ae05ca52-be37-4592-8629-06915a2470de',
+            name: 'Cliente',
+            email: 'client@gmail.com',
+            password_hash: 'senhaHash2',
+            role: 'CUSTOMER',
+            carts: {}
+        }
+    })
     // Criar produtos
 }
 
